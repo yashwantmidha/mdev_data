80868875fc6a16c35a3cb6c3fbf8d71c9ed34bd7
yashwantmidha <yashwantmidha@gmail.com>
2019-08-11 22:35:50 -0400

    Initial

---
+++ mobiledata/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+using GraphQL.Client;
+using mobiledata.Model;
+
+
+namespace mobiledata
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var client = new GraphQLClient("https://swapi.apis.guru");
+
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms {films { id, title, director } } }"
+
+            };
+            GraphQLResponse graphQLResponse = await client.PostAsync(graphQLRequest);
+
+            filmlist.ItemsSource = graphQLResponse.Data.allFilms.films.ToObject<List<films>>();
+        }
+    }
+}
---
+++ mobiledata/Model/films.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace mobiledata.Model
+{
+    public class films
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+    }
+}
